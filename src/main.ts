affb1b8ce9442df28156053d118737f99fb5d9a2
Juan Ochando <juanochando00@gmail.com>
2024-10-18 16:15:17 +0200

    up

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   app.setGlobalPrefix('/venwallet/api/v1');
 
   const config = new DocumentBuilder()
-    .setTitle('API Gateway v2')
+    .setTitle('API Gateway v1')
     .setDescription('API Gateway Documentation')
     .setVersion('1.0')
     .build();
